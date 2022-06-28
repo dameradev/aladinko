77dd95634af4309d2806df18ba9a849c224b7b6c
Damjan Radev <radevdamjan@gmail.com>
2022-06-28 19:56:09 +0200

    Added mdx for blog, and first post

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
+    
 
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Josefin Sans`,
+          `Open Sans`,
           `Josefin Sans\:100`,
           `Josefin Sans\:200`,
           // `montserrat`, `montserrat\:700`, `montserrat\:900`, `open sans`
@@ -54,37 +55,7 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-207972971-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        // cookieDomain: "example.com",
-        // defaults to false
-        enableWebVitalsTracking: true,
-      },
-    },
+    
     {
       resolve: `gatsby-plugin-modal-routing`,
       options: {
@@ -99,6 +70,30 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        plugins: [`gatsby-remark-images`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
 
     {
       resolve: `gatsby-source-filesystem`,
@@ -106,7 +101,7 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    
+    
   ],
 };
